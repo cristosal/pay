01448b555d026c990b0d3acc462168953a8ef2fc
cristobalsalazar <chris.salza@gmail.com>
2023-12-04 13:55:21 -0700

    rm orm id refs

--- provider.go
+++ provider.go
@@ -3,8 +3,6 @@ package pay
 import (
 	"errors"
 	"net/http"
-
-	"github.com/cristosal/orm"
 )
 
 var (
@@ -15,8 +13,6 @@ var (
 type Provider interface {
 	Init() error
 	Sync() error
-	AddCustomer(uid orm.ID, name, email string) (*Customer, error)
-	PlanByUser(uid orm.ID) (*Plan, error)
 	VerifyCheckout(string) error
 	Checkout(*CheckoutRequest) (url string, err error)
 	Webhook() http.HandlerFunc
