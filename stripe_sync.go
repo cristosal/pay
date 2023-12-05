ff34a286c8a0e70bebc30e623c515a7dda6db877
cristobalsalazar <chris.salza@gmail.com>
2023-12-04 20:11:12 -0700

    finish syncs

--- stripe_sync.go
+++ stripe_sync.go
@@ -2,10 +2,11 @@ package pay
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"log"
 
-	"github.com/stripe/stripe-go/v74"
+	"github.com/cristosal/orm"
 	"github.com/stripe/stripe-go/v74/customer"
 	"github.com/stripe/stripe-go/v74/price"
 	"github.com/stripe/stripe-go/v74/product"
@@ -38,21 +39,30 @@ func (s *StripeService) syncPrices(ctx context.Context) error {
 
 	for it.Next() {
 		p := it.Price()
-		plan, _ := s.Entities().GetPlanByProvider(ProviderStripe, p.Product.ID)
 
-		if plan == nil {
+		pr, err := s.convertPrice(p)
+		if err != nil {
+			log.Printf("error converting price %s: %v", p.ID, err)
 			continue
 		}
 
-		if err := s.Entities().AddPrice(&Price{
-			PlanID:     plan.ID,
-			Amount:     p.UnitAmount,
-			Provider:   ProviderStripe,
-			ProviderID: p.ID,
-			Currency:   string(p.Currency),
-			Schedule:   s.convertPricingSchedule(p),
-		}); err != nil {
-			return err
+		_, err = s.Entities().GetPriceByProvider(ProviderStripe, p.ID)
+
+		if errors.Is(err, orm.ErrNotFound) {
+			if err := s.Entities().UpdatePriceByProvider(pr); err != nil {
+				log.Printf("error updating price %s: %v", pr.ProviderID, err)
+			}
+			continue
+		}
+
+		if err != nil {
+			log.Printf("error getting price %s: %v", p.ID, err)
+			continue
+		}
+
+		if err := s.Entities().AddPrice(pr); err != nil {
+			log.Printf("error adding price %s: %v", pr.ProviderID, err)
+			continue
 		}
 	}
 
@@ -63,12 +73,7 @@ func (s *StripeService) syncCustomers() error {
 	it := customer.List(nil)
 	for it.Next() {
 		cust := it.Customer()
-		c := &Customer{
-			Provider:   ProviderStripe,
-			ProviderID: cust.ID,
-			Name:       cust.Name,
-			Email:      cust.Email,
-		}
+		c := s.convertCustomer(cust)
 
 		found, _ := s.Entities().GetCustomerByProvider(ProviderStripe, cust.ID)
 		if found == nil {
@@ -79,12 +84,8 @@ func (s *StripeService) syncCustomers() error {
 		}
 
 		c.ID = found.ID
-		// avoid db queries and check if we really have to update the customer
-		// we can only change email or name from stripe portal all other fields are internal
-		if c.Email != found.Email || c.Name != found.Name {
-			if err := s.Entities().UpdateCustomerByID(c); err != nil {
-				log.Printf("error while updating stripe customer with id %s: %v", c.ProviderID, err)
-			}
+		if err := s.Entities().UpdateCustomerByProvider(c); err != nil {
+			log.Printf("error while updating stripe customer with id %s: %v", c.ProviderID, err)
 		}
 	}
 
@@ -96,69 +97,64 @@ func (s *StripeService) syncCustomers() error {
 }
 
 func (s *StripeService) syncPlans() error {
-	plans, err := s.fetchPlans()
-	if err != nil {
-		return err
-	}
+	it := product.List(nil)
 
-	for _, p := range plans {
-		// check if we have it
-		found, _ := s.Entities().GetPlanByProvider(p.Provider, p.ProviderID)
+	for it.Next() {
+		p := it.Product()
+		pl := s.convertProduct(p)
 
-		if found == nil {
-			if err := s.Entities().AddPlan(&p); err != nil {
-				log.Printf("error adding plan during sync: %v", err)
+		// we need to see if we already have it
+		_, err := s.Entities().GetPlanByProvider(ProviderStripe, p.ID)
+
+		if errors.Is(err, orm.ErrNotFound) {
+			if err := s.Entities().AddPlan(pl); err != nil {
+				log.Printf("error while adding plan %s: %v", p.ID, err)
 			}
-
-			// get next
 			continue
 		}
 
-		p.ID = found.ID
-		if err := s.Entities().UpdatePlanByID(&p); err != nil {
-			log.Printf("error updating plan during sync: %v", err)
+		if err != nil {
+			log.Printf("error while getting plan %s: %v", p.ID, err)
+			continue
+		}
+
+		if err := s.Entities().UpdatePlanByProvider(pl); err != nil {
+			log.Printf("error updating plan %s: %v", pl.ProviderID, err)
 		}
 	}
 
-	return nil
+	return it.Err()
 }
 
 // syncSubscriptions pulls in all subscriptions from stripe
 func (s *StripeService) syncSubscriptions() error {
 	it := subscription.List(nil)
 	for it.Next() {
-		_ = it.Subscription()
-		// TODO: upsert subscriptions
-	}
+		sub := it.Subscription()
+		subscr, err := s.convertSubscription(sub)
+		if err != nil {
+			log.Printf("error converting subscription %s: %v", sub.ID, err)
+		}
 
-	return it.Err()
-}
-
-// fetchPlans from stripe
-func (s *StripeService) fetchPlans() ([]Plan, error) {
-	params := &stripe.ProductListParams{}
-	params.AddExpand("data.default_price")
-
-	it := product.List(params)
-
-	var plans []Plan
-	for it.Next() {
-		p := it.Product()
-		if p.DefaultPrice == nil {
+		_, err = s.Entities().GetSubscriptionByProvider(ProviderStripe, sub.ID)
+		if errors.Is(err, orm.ErrNotFound) {
+			// we add it
+			if err := s.Entities().AddSubscription(subscr); err != nil {
+				log.Printf("error adding subscription %s: %v", subscr.ProviderID, err)
+			}
 			continue
 		}
 
-		plans = append(plans, Plan{
-			ProviderID: p.ID,
-			Provider:   ProviderStripe,
-			Name:       p.Name,
-			Active:     p.Active,
-		})
+		if err != nil {
+			log.Printf("error getting subscription %s: %v", sub.ID, err)
+			continue
+		}
+
+		if err := s.Entities().UpdateSubscriptionByProvider(subscr); err != nil {
+			log.Printf("error updating subscription %s: %v", subscr.ProviderID, err)
+			continue
+		}
 	}
 
-	if it.Err() != nil {
-		return nil, it.Err()
-	}
-
-	return plans, nil
+	return it.Err()
 }
